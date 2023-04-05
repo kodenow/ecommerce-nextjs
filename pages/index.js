24b18a47c7a259883d5e3c8fb6fc7d5bbddffc2a
Kris <kodenow13@gmail.com>
2023-04-05 23:22:09 +0800

    added dynamic contents

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,36 @@
-import React from "react";
 import { Product, FooterBanner, HeroBanner } from "@/components";
 
-const Home = () => {
+import { client } from "@/lib/client";
+
+const Home = ({ products, bannerData }) => {
   return (
     <>
-      <HeroBanner />
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+
       <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Speakers of great quality</p>
       </div>
       <div className="products-container">
-        {["Product 1", "Product 2"].map((product) => product)}
+        {products?.map((product) => (
+          <Product key={product._id} product={product} />
+        ))}
       </div>
-      <FooterBanner />
+      <FooterBanner footerBanner={bannerData && bannerData[0]} />
     </>
   );
+};
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+  console.log(products);
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData },
+  };
 };
 
 export default Home;
