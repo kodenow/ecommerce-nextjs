1cdaf0e5c78f5b2493800e652a3e69bfe94d6456
Kris <kodenow13@gmail.com>
2023-04-13 12:59:55 +0800

    stripe dev working

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -2,29 +2,52 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
+    console.log(req.body);
     try {
       const params = {
-        line_items: [
-          {
-            // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-            price: "{{PRICE_ID}}",
-            quantity: 1,
-          },
-        ],
+        line_items: req.body.map((item) => {
+          console.log(item.image);
+          const img = item.image[0].asset._ref;
+          const newImage = img
+            .replace(
+              "image-",
+              "https://cdn.sanity.io/images/nuydtbop/production/"
+            )
+            .replace("-webp", ".webp");
+          console.log("Image", newImage);
+
+          return {
+            price_data: {
+              currency: "usd",
+              product_data: {
+                name: item.name,
+                images: [newImage],
+              },
+              unit_amount: item.price * 100,
+            },
+            adjustable_quantity: {
+              enabled: true,
+              minimum: 1,
+            },
+            quantity: item.quantity,
+          };
+        }),
         submit_type: "pay",
         mode: "payment",
         payment_method_types: ["card"],
         billing_address_collection: "auto",
-        shipping_options :[
-            {shipping_rate: 'shr_1Mw3zcIZqVHkqCK3VNDpbfM3'},
-            {shipping_rate: 'shr_1Mw40xIZqVHkqCK3U5m7Ts80'},
-        ]
+        shipping_address_collection: { allowed_countries: ["US", "CA"] },
+        shipping_options: [
+          { shipping_rate: "shr_1Mw3zcIZqVHkqCK3VNDpbfM3" },
+          { shipping_rate: "shr_1Mw40xIZqVHkqCK3U5m7Ts80" },
+        ],
         success_url: `${req.headers.origin}/?success=true`,
         cancel_url: `${req.headers.origin}/?canceled=true`,
       };
       // Create Checkout Sessions from body params.
       const session = await stripe.checkout.sessions.create(params);
-      res.redirect(303, session.url);
+      res.status(200).json(session);
+      // res.redirect(303, session.url);
     } catch (err) {
       res.status(err.statusCode || 500).json(err.message);
     }
